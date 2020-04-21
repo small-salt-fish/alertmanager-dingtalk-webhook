dde4a77bec9bbbd9b4dd44e64c0be2a54366a0c2
zhaohx <1532789548@qq.com>
2020-04-21 18:53:42 +0800

    3

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -26,10 +26,10 @@ func TransformToMarkdown(notification model.Notification) (markdown *model.DingT
 	for _, alert := range notification.Alerts {
 		annotations := alert.Annotations
 //		buffer.WriteString(fmt.Sprintf("##### %s\n > %s\n", annotations["summary"], annotations["description"]))
-		buffer.WriteString(fmt.Sprintf("> 告警名称: %s", annotations["summary"]))
-		buffer.WriteString(fmt.Sprintf("\n> 告警状态: %s", status))
-		buffer.WriteString(fmt.Sprintf("\n> 告警详情：%s", annotations["description"]))
-		buffer.WriteString(fmt.Sprintf("\n> 告警时间：%s", alert.StartsAt.Format("15:04:05")))
+		buffer.WriteString(fmt.Sprintf("+ 告警名称: %s", annotations["summary"]))
+		buffer.WriteString(fmt.Sprintf("+ 告警状态: %s", status))
+		buffer.WriteString(fmt.Sprintf("+ 告警详情：%s", annotations["description"]))
+		buffer.WriteString(fmt.Sprintf("+ 告警时间：%s", alert.StartsAt.Format("15:04:05")))
 	}
 
 	markdown = &model.DingTalkMarkdown{
