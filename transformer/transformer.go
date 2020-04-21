5fc3c8a9545c27021268035bf5d9f31cc3ac4b2a
zhaohx <1532789548@qq.com>
2020-04-21 14:36:49 +0800

    transformer.go

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -18,20 +18,24 @@ func TransformToMarkdown(notification model.Notification) (markdown *model.DingT
 
 	var buffer bytes.Buffer
 
-	buffer.WriteString(fmt.Sprintf("### 通知组%s(当前状态:%s) \n", groupKey, status))
+//	buffer.WriteString(fmt.Sprintf("### 通知组%s(当前状态:%s) \n", groupKey, status))
+	buffer.WriteString(fmt.Sprintf("### 告警通知: "))
 
-	buffer.WriteString(fmt.Sprintf("#### 告警项:\n"))
+//	buffer.WriteString(fmt.Sprintf("#### 告警项:\n"))
 
 	for _, alert := range notification.Alerts {
 		annotations := alert.Annotations
-		buffer.WriteString(fmt.Sprintf("##### %s\n > %s\n", annotations["summary"], annotations["description"]))
+//		buffer.WriteString(fmt.Sprintf("##### %s\n > %s\n", annotations["summary"], annotations["description"]))
+		buffer.WriteString(fmt.Sprintf("##### 告警名称: %s\n", annotations["summary"]))
+		buffer.WriteString(fmt.Sprintf("##### 告警详情：%s\n", annotations["description"]))
 		buffer.WriteString(fmt.Sprintf("\n> 开始时间：%s\n", alert.StartsAt.Format("15:04:05")))
 	}
 
 	markdown = &model.DingTalkMarkdown{
 		MsgType: "markdown",
 		Markdown: &model.Markdown{
-			Title: fmt.Sprintf("通知组：%s(当前状态:%s)", groupKey, status),
+//			Title: fmt.Sprintf("通知组：%s(当前状态:%s)", groupKey, status),
+			Title: fmt.Sprintf("告警通知："),
 			Text:  buffer.String(),
 		},
 		At: &model.At{
