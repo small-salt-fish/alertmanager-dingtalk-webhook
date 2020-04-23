969e589f6f0a7bc658763806e494ff78c315ff75
zhaohx <1532789548@qq.com>
2020-04-23 14:49:21 +0800

    3

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -34,7 +34,7 @@ func TransformToMarkdown(notification model.Notification) (markdown *model.DingT
 		buffer.WriteString(fmt.Sprintf("+ 告警实例: %s\n", labels["instance"]))
 		buffer.WriteString(fmt.Sprintf("+ 告警级别: %s\n", labels["severity"]))
 		buffer.WriteString(fmt.Sprintf("+ 告警时间: %s\n", alert.StartsAt.Format("15:04:05")))
-		buffer.WriteString(fmt.Sprintf("+ 告警详情: %s", annotations["description"]))
+		buffer.WriteString(fmt.Sprintf("+ 告警详情: %s\n", annotations["description"]))
 		buffer.WriteString(fmt.Sprintf("+ 告警地址: [链接详情](%s)", url))
 	}
 
